b6d6ec5578e5f1de50fe63bdc3b5d0cb661afbf4
yu <peacetomori1118@gmail.com>
2019-10-05 03:20:07 +0900

    temporary prevent scroll

--- script.js
+++ script.js
@@ -10,14 +10,31 @@ function locationLeave(locationName) {
     child.style.display = 'none';
 }
 
-function triggerModal(event) {
-    var modal = document.getElementById('header-menu-modal');
-    modal.style.display = 'block';
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-}
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('modal-button').addEventListener('click', function (e) {
+        document.getElementById('header-menu-modal').style.display = 'block';
+        preventScroll.enable();
+    }, false);
+}, false);
+
+var preventScroll = {
+    x:0,
+    y:0,
+    setPos(x=window.pageXOffset,y=window.pageYOffset){
+        this.x=x;
+        this.y=y;
+    },
+    handleEvent(){
+        window.scrollTo(this.x,this.y);
+    },
+    enable(){
+        this.setPos();
+        window.addEventListener("scroll",this);
+    },
+    disable(){
+        window.removeEventListener("scroll",this);
+    }
+};
 
 function scrollFunc(content) {
     var modal = document.getElementById('header-menu-modal');
@@ -25,15 +42,18 @@ function scrollFunc(content) {
     var contentElement = document.getElementById(content);
     var domRect = contentElement.getBoundingClientRect();
     var loc = domRect.top;
+
+    preventScroll.disable();
     window.scrollTo({
         top: window.pageYOffset + loc - 100,
         behavior: "smooth"
     });
 }
 
-function stopModal() {
+function stopModal(event) {
     var modal = document.getElementById('header-menu-modal');
     modal.style.display = 'none';
+    preventScroll.disable();
 }
 
 function faqFunc(n) {
