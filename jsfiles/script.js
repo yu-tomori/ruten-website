a5f35d2d8462c89b8338810e85fac54ebcbb957f
yu <peacetomori1118@gmail.com>
2019-10-21 18:24:12 +0900

    service hover

--- jsfiles/script.js
+++ jsfiles/script.js
@@ -1,22 +1,3 @@
-function locationOver(locationName) {
-    var loc = document.getElementById('location-' + locationName);
-    var child = loc.firstChild.nextElementSibling;
-    child.style.display = 'block';
-}
-
-function locationLeave(locationName) {
-    var loc = document.getElementById('location-' + locationName);
-    var child = loc.firstChild.nextElementSibling;
-    child.style.display = 'none';
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('modal-button').addEventListener('click', function (e) {
-        document.getElementById('header-menu-modal').style.display = 'block';
-        preventScroll.enable();
-    }, false);
-}, false);
-
 var preventScroll = {
     x:0,
     y:0,
@@ -54,19 +35,6 @@ function stopModal(event) {
     var modal = document.getElementById('header-menu-modal');
     modal.style.display = 'none';
     preventScroll.disable();
-}
-
-function faqFunc(n) {
-    var answer = document.getElementById('faq-a-' + n);
-    var dropdownMark = answer.previousElementSibling.lastChild;
-    if (answer.style.display == 'none') {
-        answer.style.display = 'block';
-        dropdownMark.src = './icon/dropdown-icon-up-direction.png';
-    } else {
-        answer.style.display = 'none';
-        dropdownMark.src = './icon/dropdown-icon-down-direction.png';
-
-    }
 }
 
 window.onscroll = function() {
